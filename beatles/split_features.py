b17613415f03eb84f4ec40e2d1c1338a3b28e298
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-04-16 17:53:30 +0200

    Refactor split_feature.py a bit.
    - avoid global variables
    - pass the file/directory names as parameter
    - call the main function (if ran as a script)

--- beatles/split_features.py
+++ beatles/split_features.py
@@ -14,13 +14,10 @@ import numpy as np
 import pandas as pd
 import os
 
-index_file = 'data/beatles/songs-dataset-split.tsv'
-df_index = pd.read_csv(index_file, sep='\t')
-
-df_index.sort('order', inplace=True)
-
-labels_dir = 'data/beatles/chord-pcs/4096_2048/'
-features_dir = 'data/beatles/chromagram/block=4096_hop=2048_bins=-48,67_div=1/'
+def load_index(file_name):
+    df_index = pd.read_csv(file_name, sep='\t')
+    df_index.sort('order', inplace=True)
+    return df_index
 
 def load_song(song_path):
     df_labels = pd.read_csv(labels_dir + song_path + '.pcs', sep='\t')
@@ -39,23 +36,24 @@ def load_songs(df_index):
         Y.append(y)
     return np.vstack(X), np.vstack(Y)
 
-target_dir = 'data/beatles/ml_dataset/chromagram_block=4096_hop=2048_bins=-48,67_div=1/'
-
-def merge_split(split):
-    print('--- split: ', split, '---')
-    X, Y = load_songs(df_index[df_index['split'] == split])
-
-def split_songs_and_save_to_single_file():
+def split_songs_and_save_to_single_file(index_file, labels_dir, features_dir, target_file):
+    df_index = load_index(index_file)
     splits = {}
     for split in ('train', 'valid', 'test'):
         print('--- split: ', split, '---')
         X, Y = load_songs(df_index[df_index['split'] == split])
         splits['X_' + split] = X
         splits['Y_' + split] = Y
-    target_file = target_dir + 'dataset.npz'
+
     print('saving to:', target_file)
+    target_dir = os.path.dirname(target_file)
     os.makedirs(target_dir, exist_ok=True)
     np.savez_compressed(target_file, **splits)
 
 if __name__ == '__main__':
-    split_songs_and_save_to_single_file()
+    index_file = 'data/beatles/songs-dataset-split.tsv'
+    labels_dir = 'data/beatles/chord-pcs/4096_2048/'
+    features_dir = 'data/beatles/chromagram/block=4096_hop=2048_bins=-48,67_div=1/'
+    target_file = 'data/beatles/ml_dataset/chromagram_block=4096_hop=2048_bins=-48,67_div=1/dataset.npz'
+
+    split_songs_and_save_to_single_file(index_file, labels_dir, features_dir, target_file)
