04754d463f32c636d1f735b13eb9935f1136aa2b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-05-15 21:55:06 +0200

    Fix a script to train CNN+LSTM model which was exported from Jupyter.
    - make it running
    - save model using generated model id
    - checkpoint saving
    - store the training history info to a TSV file
    - store error analysis plot from an excerpt to a PNG file
    
    It is nasty and many things are duplicate with the pure CNN model.
    It' going to be cleaned. This step is to make it replicable.

--- chord-recognition/lstm_chord_classification_training.py
+++ chord-recognition/lstm_chord_classification_training.py
@@ -1,31 +1,49 @@
+"""
+Training a model to classify chord (pitch class set) labels from chormagram
+features. The architecture consists of a few 1D convolutional layers followed
+by LSTM and 12 sigmoids at the end (for multi-label classification).
 
-# coding: utf-8
+On GTX 980 Ti this model eats 5900 MB memory and one epoch takes ~ 66 seconds.
+"""
 
-# In[1]:
-
-get_ipython().magic('matplotlib inline')
+import arrow
+from keras.callbacks import ModelCheckpoint
+from keras.layers import TimeDistributed
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution1D, MaxPooling1D, Convolution2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+import math
 import matplotlib as mpl
+# do not use Qt/X that require $DISPLAY
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
 import seaborn as sns
+from sklearn.metrics import hamming_loss, accuracy_score, roc_auc_score
+
 
 mpl.rc('image', interpolation='nearest', origin='lower', aspect='auto', cmap='bwr')
 mpl.rc('axes', grid=False)
 mpl.rc('figure', figsize=(10,5))
 
-from sklearn.metrics import hamming_loss, accuracy_score, roc_auc_score
 
+data_dir = '../data'
 
-# In[2]:
+max_seq_size = 100
+batch_size = 32
+epoch_count = 3
 
-dataset = np.load('../data/beatles/ml_dataset/chromagram_block=4096_hop=2048_bins=-48,67_div=1/dataset.npz')
+dataset = np.load(data_dir + '/beatles/ml_dataset/block=4096_hop=2048_bins=-48,67_div=1/dataset_2016-05-15.npz')
 
-X_train_orig, Y_train_orig, X_valid_orig, Y_valid_orig, X_test_orig, Y_test_orig =     dataset['X_train'], dataset['Y_train'],     dataset['X_valid'], dataset['Y_valid'],     dataset['X_test'], dataset['Y_test']
+X_train_orig, Y_train_orig, X_valid_orig, Y_valid_orig, X_test_orig, Y_test_orig = dataset['X_train'], dataset['Y_train'],     dataset['X_valid'], dataset['Y_valid'],     dataset['X_test'], dataset['Y_test']
 
 feature_count = X_train_orig.shape[1]
 target_count = Y_train_orig.shape[1]
-    
+
 # let's rescale the features manually so that the're the same in all songs
 # the range (in dB) is -120 to X.shape[1] (= 115)
 def normalize(X):
@@ -38,21 +56,25 @@ X_test_orig = normalize(X_test_orig)
 for d in [X_train_orig, X_valid_orig, X_test_orig, Y_train_orig, Y_valid_orig, Y_test_orig]:
     print(d.shape)
 
-
-# In[3]:
-
 # we'll cut the datasets into small sequences of frames
-max_seq_size = 100
-
-
-# In[4]:
-
-# we discard some frames (better would be to pad)
 
 def cut_sequences(a, max_seq_size):
     n = len(a)
     n_cut = len(a) - len(a) % max_seq_size
     return a[:n_cut].reshape(-1, max_seq_size, a.shape[1])
+
+# there might be some problem which lead to bad results...
+def pad_sequences(a, max_seq_size):
+    """
+    Cuts the list of frames into fixed-length sequences.
+    The end is padded with zeros if needed.
+    (frame_count, feature_count) -> (seq_count, max_seq_size, feature_count)
+    """
+    n = len(a)
+    n_padded = max_seq_size * (math.ceil(n / max_seq_size))
+    a_padded = np.zeros((n_padded, a.shape[1]), a.dtype)
+    a_padded[:n, :] = a
+    return a_padded.reshape(-1, max_seq_size, a.shape[1])
 
 X_train_seq = cut_sequences(X_train_orig, max_seq_size)
 X_valid_seq = cut_sequences(X_valid_orig, max_seq_size)
@@ -61,41 +83,17 @@ Y_valid_seq = cut_sequences(Y_valid_orig, max_seq_size)
 
 for d in [X_train_seq, X_valid_seq, Y_train_seq, Y_valid_seq]:
     print(d.shape)
-    
+
 Y_train_flat = Y_train_seq.reshape(-1, target_count)
 Y_valid_flat = Y_valid_seq.reshape(-1, target_count)
 
-
-# In[5]:
-
 X_train_seq_conv = X_train_seq.reshape(X_train_seq.shape[0], max_seq_size, feature_count, 1)
 X_valid_seq_conv = X_valid_seq.reshape(X_valid_seq.shape[0], max_seq_size, feature_count, 1)
-
-
-# In[6]:
 
 X_train = X_train_seq_conv
 X_valid = X_valid_seq_conv
 Y_train = Y_train_seq
 Y_valid = Y_valid_seq
-
-
-# In[7]:
-
-from keras.models import Sequential
-from keras.layers import TimeDistributed
-from keras.layers.recurrent import LSTM
-from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Convolution1D, MaxPooling1D, Convolution2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
-
-
-# In[8]:
-
-batch_size = 32
-
-
-# In[9]:
 
 # model = Sequential()
 # model.add(LSTM(output_dim=50,
@@ -284,42 +282,62 @@ model.add(LSTM(64, return_sequences=True))
 model.add(Dropout(0.25))
 model.add(TimeDistributed(Dense(12, activation='sigmoid')))
 
-
-# In[10]:
-
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print('param count:', model.count_params())
 print('input shape:', model.input_shape)
 print('output shape:', model.output_shape)
 
+def new_model_id():
+    return 'model_%s' % arrow.get().format('YYYY-MM-DD-HH-mm-ss')
 
-# In[11]:
+def save_model_arch(model_id, model):
+    arch_file = '%s/%s_arch.json' % (model_dir, model_id)
+    print('Saving model architecture:', arch_file)
+    open(arch_file, 'w').write(model.to_json())
+
+def weights_file(model_id, suffix=''):
+    return '%s/%s_weights%s.h5' % (model_dir, model_id, suffix)
+
+model_id = new_model_id()
+print('model id:', model_id)
+
+model_dir = data_dir + '/beatles/models/' + model_id
+os.makedirs(model_dir, exist_ok=True)
+
+save_model_arch(model_id, model)
 
 ## training for stateful RNNs - requires reseting model states:
 ## shuffle=False - so that ordering is maintained
 # for i in range(10):
-#     model.fit(X_train_seq, Y_train_seq, 
+#     model.fit(X_train_seq, Y_train_seq,
 #               #validation_data=(X_valid_seq, Y_valid_seq),
 #               nb_epoch=1, batch_size=batch_size, shuffle=False)
 #     model.reset_states()
 
+checkpointer = ModelCheckpoint(filepath=weights_file(model_id, '_checkpoint'), verbose=1, save_best_only=True)
 
-# In[12]:
+print('Training the model')
 
-hist = model.fit(X_train, Y_train,
+training_hist = model.fit(X_train, Y_train,
           validation_data=(X_valid, Y_valid),
-          nb_epoch=10, batch_size=batch_size)
+          nb_epoch=epoch_count,
+          batch_size=batch_size,
+          verbose=1,
+          callbacks=[checkpointer])
 
+print('Model has been trained')
 
-# In[13]:
+model.save_weights(weights_file(model_id, ''))
 
-for label in sorted(hist.history):
-    plt.plot(hist.history[label], label=label)
-plt.legend();
+history = training_hist.history
+history_file = '%s/%s_training_history.tsv' % (model_dir, model_id)
+print('Learning curve:', history_file)
+pd.DataFrame(history).to_csv(history_file, header=True)
 
-
-# In[14]:
+# for label in sorted(training_hist.history):
+#     plt.plot(training_hist.history[label], label=label)
+# plt.legend()
 
 def predict(model, X):
     Y_proba = model.predict(X, batch_size=batch_size, verbose=1).reshape(-1, target_count)
@@ -329,13 +347,7 @@ def predict(model, X):
 Y_train_proba, Y_train_classes = predict(model, X_train)
 Y_valid_proba, Y_valid_classes = predict(model, X_valid)
 
-
-# In[15]:
-
-Y_train_proba.shape
-
-
-# In[16]:
+print(Y_train_proba.shape)
 
 def evaluate_model(model, Y_true, Y_pred_classes, Y_pred_proba):
     print('accuracy:', accuracy_score(Y_true, Y_pred_classes))
@@ -347,32 +359,17 @@ evaluate_model(model, Y_train_flat, Y_train_classes, Y_train_proba)
 print('-- validation:')
 evaluate_model(model, Y_valid_flat, Y_valid_classes, Y_valid_proba)
 
+## Fails with: "Intel MKL FATAL ERROR: Cannot load libmkl_mc3.so or libmkl_def.so."
+# def hamming_score_dist_by_samples(Y_true, Y_pred_classes):
+#     Y_pred_classes_seq = Y_pred_classes.reshape(-1, max_seq_size, target_count)
+#     return np.array([1 - hamming_loss(Y_true[i], Y_pred_classes_seq[i]) for i in range(Y_true.shape[0])])
+#
+# sns.distplot(hamming_score_dist_by_samples(Y_train, Y_train_classes), bins=20, label='train')
+# sns.distplot(hamming_score_dist_by_samples(Y_valid, Y_valid_classes), bins=20, label='valid')
+# plt.xlim(0, 1)
+# plt.legend()
 
-# In[17]:
-
-def hamming_score_dist_by_samples(Y_true, Y_pred_classes):
-    Y_pred_classes_seq = Y_pred_classes.reshape(-1, max_seq_size, target_count)
-    return np.array([1 - hamming_loss(Y_true[i], Y_pred_classes_seq[i]) for i in range(Y_true.shape[0])])
-
-sns.distplot(hamming_score_dist_by_samples(Y_train, Y_train_classes), bins=20, label='train')
-sns.distplot(hamming_score_dist_by_samples(Y_valid, Y_valid_classes), bins=20, label='valid')
-plt.xlim(0, 1)
-plt.legend();
-
-
-# In[18]:
-
-# np.savetxt('yesterday_lstm_class.tsv', y_pred, delimiter='\t', fmt='%d')
-# np.savetxt('yesterday_lstm_proba.tsv', y_pred_proba, delimiter='\t')
-
-
-# In[19]:
-
-# np.savetxt('all_lstm_class.tsv', Y_train_pred, delimiter='\t', fmt='%d')
-# np.savetxt('all_lstm_proba.tsv', Y_train_pred_proba, delimiter='\t')
-
-
-# In[37]:
+print('Plotting error analysis on an excerpt of dataset')
 
 excerpt_start = 0
 excerpt_len = 1000 #len(Y_valid.reshape(-1, 12))
@@ -403,28 +400,8 @@ ax4.set_title('error: probability')
 
 ax5.imshow(y_pred_classes - y_true, vmin=-1, vmax=1)
 ax5.set_yticks(range(12))
-ax5.set_title('error: classes');
+ax5.set_title('error: classes')
 
-
-# In[43]:
-
-# with open('model_cnn_lstm_01.json', 'w') as file: file.write(model.to_json())
-
-
-# In[24]:
-
-# model.save_weights('model_cnn_lstm_01_weights.h5')
-
-
-# In[65]:
-
-# def load_model(arch_file, weights_file):
-#     from keras.models import model_from_json
-#     with open(arch_file, 'r') as file:
-#         model = model_from_json(file.read())
-#         model.load_weights(weights_file)
-#         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#         return model
-    
-# model2 = load_model('model_cnn_lstm_01.json', 'model_cnn_lstm_01_weights.h5')
-
+error_analysis_file = '%s/%s_error_analysis.png' % (model_dir, model_id)
+print(error_analysis_file)
+plt.savefig(error_analysis_file)
