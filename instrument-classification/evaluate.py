cb8872bebdcf426e2fe9ad316b8a37303919fce0
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-13 16:45:45 +0200

    Integrate analysis of error by pitch to the main evaluation script.

--- instrument-classification/evaluate.py
+++ instrument-classification/evaluate.py
@@ -46,6 +46,7 @@ def evaluate_model(data_dir, model_dir, evaluation_dir):
 
     def plot_confusion_matrix(cm, labels, split):
         plt.figure()
+        plt.grid(False)
         plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
         plt.title('Confusion matrix (%s)' % split)
         plt.colorbar()
@@ -66,6 +67,47 @@ def evaluate_model(data_dir, model_dir, evaluation_dir):
         csv_file = evaluation_dir + '/per_class_metrics_%s.csv' % split
         per_class_metrics.to_csv(csv_file, float_format='%.3f')
 
+    def analyze_error_by_pitch():
+        parameters_with_targets = pd.read_csv(data_dir + '/parameters_with_targets.csv', index_col=0)
+
+        data_points = parameters_with_targets.join(predictions)
+
+        def compute_error_by_midi_bin(split='valid'):
+            correct_by_midi_valid = data_points[data_points['split'] == split][['midi_number', 'accurate']]
+
+            df = pd.DataFrame(index=np.arange(128))
+            hist = correct_by_midi_valid.groupby(['midi_number', 'accurate']).size().reset_index().set_index('midi_number')
+            df['correct'] = hist[hist['accurate']][0]
+            df['incorrect'] = hist[~hist['accurate']][0]
+            df.fillna(0, inplace=True)
+
+            df['bin'] = df.index.map(lambda i: (i // 8) * 8)
+            df['total'] = df['correct'] + df['incorrect']
+
+            df_bins = df.groupby('bin').sum()
+
+            df_bins['correct_perc'] = df_bins['correct'] / df_bins['total']
+            df_bins['incorrect_perc'] = df_bins['incorrect'] / df_bins['total']
+
+            csv_file = evaluation_dir + '/error_by_midi_bins_%s.csv' % split
+            df_bins.to_csv(csv_file)
+
+            return df_bins
+
+        def plot_error_by_midi_bin(df, split):
+            fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
+            df[['correct', 'incorrect']].plot(
+                kind='bar', stacked=True, color=['green', 'red'], ax=axes[0])
+            df[['correct_perc', 'incorrect_perc']].plot(
+                kind='bar', stacked=True, color=['green', 'red'], ax=axes[1])
+
+            plt.suptitle('Error (absolute, relative) by pitch bins')
+
+            plt.savefig(evaluation_dir + '/error_by_midi_bins_%s.pdf' % split)
+
+        for split in splits:
+            plot_error_by_midi_bin(compute_error_by_midi_bin(split), split)
+
     plot_learning_curves(training_history)
 
     for split in splits:
@@ -74,6 +116,7 @@ def evaluate_model(data_dir, model_dir, evaluation_dir):
         plot_confusion_matrix(cm, instr_family_le.classes_, split)
         per_class_metrics(cm, split)
 
+    analyze_error_by_pitch()
 
 if __name__ == '__main__':
     base_dir = 'data/working/single-notes-2000'
