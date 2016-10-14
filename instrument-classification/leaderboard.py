e64b82d9c485bf9d52de511f9a96c1a40c07cb10
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-15 00:22:06 +0200

    #25 Add a script to select best models and rank of the last few models.

---
+++ instrument-classification/leaderboard.py
@@ -0,0 +1,29 @@
+import glob
+import numpy as np
+import pandas as pd
+
+def load_df(file_name):
+    model_id = file_name.split('/')[4]
+    df = pd.read_csv(file_name)
+    df.rename(columns={'Unnamed: 0': 'split'}, inplace=True)
+    df['model_id'] = model_id
+    if 'auc' not in df.columns:
+        df['auc'] = np.nan
+    df = df[['model_id', 'split', 'loss', 'error', 'auc']]
+    return df
+
+files = glob.glob('data/working/single-notes-2000/models/*/evaluation/final_metrics.csv')
+df = pd.concat([load_df(f) for f in files])
+
+df_valid = df[df['split'] == 'valid']
+df_valid.set_index('model_id', inplace=True)
+
+print('Ranking on the validation split metrics.\n')
+print('Lowest error:')
+print(df_valid.sort_values('error', ascending=True)[:5])
+
+print('\nHighest AUC:')
+print(df_valid.sort_values('auc', ascending=False)[:5])
+
+print('\nRank of the last few models by error, total:', len(df_valid))
+print(df_valid.rank()['error'].astype(int).iloc[-5:])
