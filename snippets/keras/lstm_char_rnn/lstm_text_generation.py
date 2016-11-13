e4168b3ca57669235b3945cd7534cdeb438af298
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-11-13 18:27:00 +0100

    Increase the batch size.

--- snippets/keras/lstm_char_rnn/lstm_text_generation.py
+++ snippets/keras/lstm_char_rnn/lstm_text_generation.py
@@ -76,7 +76,7 @@ for iteration in range(1, 60):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=128, nb_epoch=1)
+    model.fit(X, y, batch_size=1000, nb_epoch=1)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
