38342fd4a7d530f0b5da951ab582ab06ce06e14c
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-11-22 01:21:12 +0700

    Refactor the char-ran example a lot.
    - turn the dataset and model into classes
    - extract some methods
    - turn sampling the characters into a Python generator

--- snippets/keras/lstm_char_rnn/lstm_text_generation.py
+++ snippets/keras/lstm_char_rnn/lstm_text_generation.py
@@ -26,93 +26,128 @@ import random
 from sklearn.preprocessing import LabelEncoder
 import sys
 
-path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
-text = open(path).read().lower()
-print('corpus length:', len(text))
+class Dataset:
+    def __init__(self, frame_size=40, hop_size=3):
+        self.frame_size = frame_size
+        self.hop_size = hop_size
 
-chars = sorted(list(set(text)))
-class_count = len(chars)
-print('total chars:', class_count)
+        path = get_file('nietzsche.txt',
+            origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
+        self.text = open(path).read().lower()[:10000]
+        print('corpus length:', len(self.text))
 
-le = LabelEncoder().fit(chars)
-text_le = le.transform(list(text))
-text_ohe = to_categorical(text_le)
+        chars = sorted(list(set(self.text)))
+        self.class_count = len(chars)
+        print('total chars:', self.class_count)
 
-def ohe_to_text(text_ohe):
-    return ''.join(le.inverse_transform(text_ohe.argmax(axis=1)))
+        self.le = LabelEncoder().fit(chars)
 
-def split_to_frames(values, frame_size, hop_size):
-    """
-    Split to overlapping frames.
-    """
-    return np.stack(values[i:i + frame_size] for i in range(0, len(values) - frame_size + 1, hop_size))
+        self.text_ohe = self.text_to_ohe(self.text)
 
-def split_features_targets(frames):
-    """
-    Split each frame to features (all but last element)
-    and targets (last element).
-    """
-    frame_size = frames.shape[1]
-    X = frames[:, :frame_size-1]
-    y = frames[:, -1]
-    return X, y
+        def split_to_frames(values, frame_size, hop_size):
+            """
+            Split to overlapping frames.
+            """
+            return np.stack(values[i:i + frame_size] for i in
+                range(0, len(values) - frame_size + 1, hop_size))
 
-# cut the text in semi-redundant sequences of frame_size characters
-frame_size = 40
-hop_size = 3
+        def split_features_targets(frames):
+            """
+            Split each frame to features (all but last element)
+            and targets (last element).
+            """
+            frame_size = frames.shape[1]
+            X = frames[:, :frame_size - 1]
+            y = frames[:, -1]
+            return X, y
 
-X, y = split_features_targets(split_to_frames(text_ohe, frame_size+1, hop_size))
+        # cut the text in semi-redundant sequences of frame_size characters
+        self.X, self.y = split_features_targets(split_to_frames(
+            self.text_ohe, frame_size + 1, hop_size))
 
-print('X.shape:', X.shape, 'y.shape:', y.shape)
+        print('X.shape:', self.X.shape, 'y.shape:', self.y.shape)
 
-# build the model: a single LSTM
-print('Build model...')
-model = Sequential()
-model.add(LSTM(128, input_shape=(frame_size, class_count)))
-model.add(Dense(class_count))
-model.add(Activation('softmax'))
+    def ohe_to_text(self, text_ohe):
+        return self.le_to_text(text_ohe.argmax(axis=1))
 
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=optimizer)
+    def text_to_ohe(self, text):
+        return self.le_to_ohe(self.text_to_le(list(text)))
 
+    def le_to_text(self, text_le):
+        return ''.join(self.le.inverse_transform(text_le))
 
-def sample(preds, temperature=1.0):
-    # helper function to sample an index from a probability array
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
+    def text_to_le(self, text):
+        return self.le.transform(text)
 
-def preview(model, epoch):
-    start_index = random.randint(0, len(text) - frame_size - 1)
+    def le_to_ohe(self, text_le):
+        return to_categorical(text_le, nb_classes=self.class_count)
 
-    print()
-    print('Epoch:', epoch)
+class Model:
+    def __init__(self, dataset):
+        self.dataset = dataset
 
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        def create_model(dataset):
+            # build the model: a single LSTM
+            print('Build model...')
+            model = Sequential()
+            model.add(LSTM(128, input_shape=(dataset.frame_size, dataset.class_count)))
+            model.add(Dense(dataset.class_count))
+            model.add(Activation('softmax'))
+
+            optimizer = RMSprop(lr=0.01)
+            model.compile(loss='categorical_crossentropy', metrics=['accuracy'],
+                optimizer=optimizer)
+            return model
+
+        self.model = create_model(dataset)
+
+    def fit_with_preview(self):
+        # output generated text after each iteration
+        preview_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: self.preview(model, dataset))
+        model.fit(
+            self.dataset.X, self.dataset.y,
+            batch_size=1000, nb_epoch=60,
+            callbacks=[preview_callback])
+
+    def generate_chars(self, seed_text, length, temperature=1.0):
+        def sample(preds):
+            # helper function to sample an index from a probability array
+            preds = np.asarray(preds).astype('float64')
+            preds = np.log(preds) / temperature
+            exp_preds = np.exp(preds)
+            preds = exp_preds / np.sum(exp_preds)
+            probas = np.random.multinomial(1, preds, 1)
+            return np.argmax(probas)
+
+        window_ohe = self.dataset.text_to_ohe(seed_text)
+
+        for i in range(length):
+            # single data point
+            x = window_ohe[np.newaxis]
+
+            probs = model.predict(x, verbose=0)[0]
+            next_index = sample(probs, temperature)
+
+            yield dataset.le_to_text(next_index)
+
+            next_ohe = dataset.le_to_ohe([next_index])
+            window_ohe = np.vstack([window_ohe[1:], next_ohe])
+
+    def preview(self, seed_text=None, length=100):
+        if seed_text is None:
+            start_index = random.randint(0, len(dataset.text) - dataset.frame_size - 1)
+            seed_text = self.dataset.text[start_index:start_index + dataset.frame_size]
         print()
-        print('----- diversity:', diversity)
+        print('----- Generating with seed: "' + seed_text + '"')
 
-        current_context = text_ohe[start_index:start_index + frame_size]
-        print('----- Generating with seed: "' + ohe_to_text(current_context) + '"')
+        for temperature in [0.2, 0.5, 1.0, 1.2]:
+            print('----- temperature:', temperature)
+            for char in self.generate_chars(seed_text, length, temperature):
+                sys.stdout.write(char)
+                sys.stdout.flush()
+            print()
 
-        for i in range(100):
-            x = current_context[np.newaxis]
-
-            preds = model.predict(x, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_binary = to_categorical([next_index], nb_classes=class_count)
-
-            current_context = np.vstack([current_context[1:], next_binary])
-
-            sys.stdout.write(le.inverse_transform(next_index))
-            sys.stdout.flush()
-        print()
-
-# train the model, output generated text after each iteration
-batch_size = 1000
-model.fit(X, y, batch_size=batch_size, nb_epoch=60, callbacks=[
-    LambdaCallback(on_epoch_end=lambda epoch, logs: preview(model, epoch))
-])
+if __name__ == '__main__':
+    dataset = Dataset()
+    model = Model(dataset)
+    model.fit_with_preview()
